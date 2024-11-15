e1d4ba00c5d7c0c952634f901340c5f9c16bae2c
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-15 13:54:22 -0800

    Move db to app

--- src/app/api/resume/query/route.ts
+++ src/app/api/resume/query/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/db";
+import prisma from "@/app/lib_prisma/db";
 
 export async function GET(_: NextRequest) {
   try {
