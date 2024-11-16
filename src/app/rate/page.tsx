306e791b14339441e33e6393c9b9d81fc69ea55c
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-16 00:03:56 -0800

    Init fetch data

--- src/app/rate/page.tsx
+++ src/app/rate/page.tsx
@@ -1,3 +1,13 @@
+import axios from "axios"
+import { useEffect } from "react"
+
 export default function Rate() {
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get("/api/resume/query");
+            console.log(res.data);
+        }
+    })
+
     return <h1>Rate</h1>
 }
