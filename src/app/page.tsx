dcde0e170ab239900cf7d27527f81fe611027e3b
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-11-17 10:27:00 -0800

    Minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
         <button
           type="button"
           onClick={() => window.location.href = "/rate"}
-          className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 disabled:bg-blue-300"
+          className="bg-blue-500 text-white px-6 py-2 rounded-lg text-lg md:text-xl hover:bg-blue-600 disabled:bg-blue-300"
         >
           Rate Others!
         </button>
